f1a17e6e683911f6f1a5f2d6e9901da576fb0e4b
RobbySchmidt <schmidt@rhowerk.de>
2025-05-03 16:01:31 +0200

    update product card

--- components/card.vue
+++ components/card.vue
@@ -2,27 +2,21 @@
   <div class="relative rounded-md overflow-hidden text-center shadow-md md:md:hover:scale-105 duration-200 ease-in-out">
     <div class="grid grid-cols-2 gap-4 items-center p-4">
       <img class="block w-56 h-56 object-contain" 
-        :src="image" alt="">
+        :src="product.image" alt="">
       <span class="absolute top-4 right-4 bg-prime text-white rounded-sm px-3 py-1.5">
-        {{ category }}
+        {{ product.category }}
       </span>
       <span>
-        {{ title }}
+        {{ product.title }}
       </span>
     </div>
-    <span class="block py-2">Price: {{ formatPrice(price) }}€</span>
+    <span class="block py-2">Price: {{ formatPrice(product.price) }}€</span>
   </div>
 </template>
 
 <script setup>
   defineProps({
-    image: String,
-    price: {
-      type: [String, Number],
-      required: true
-    },
-    title: String,
-    category: String
+    product: Object
   })
 </script>
 
